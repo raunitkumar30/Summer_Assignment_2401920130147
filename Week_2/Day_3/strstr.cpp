e0ef62d8203bbb24ef94ea8233487620e28efdd0
Raunit Kumar <raunitkumar232333@gmail.com>
2026-06-10 19:14:54 +0530

    strStr

---
+++ Week_2/Day_3/strstr.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len1 = haystack.length();
+        int len2 = needle.length();
+        if (len1 < len2) return -1;
+        for(int i=0;i<=(len1-len2);i++){
+            if(haystack.substr(i,len2) == needle){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
